b8ac60aa7f089970fb40b481305dac4c69913c78
Aleksandr Golovin <al.golovin@yadro.com>
2025-08-16 18:04:59 +0300

    Update docker + ci

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import allure
 from selenium import webdriver
@@ -9,14 +10,42 @@ from pages.main_page import MainPage
 @allure.step('Фикстура: инициализация драйвера браузера')
 @pytest.fixture
 def driver():
+    selenoid_url = os.getenv('SELENOID_URL', 'http://localhost:4444/wd/hub')
+    
+    chrome_options = ChromeOptions()
     chrome_options = ChromeOptions()
     chrome_options.add_argument("--incognito")
     chrome_options.add_argument(f"--window-size=1400,1000")
-    driver_instance = webdriver.Chrome(options=chrome_options)
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    
+    driver_instance = webdriver.Remote(
+        command_executor=selenoid_url,
+        options=chrome_options
+    )
+    driver_instance.maximize_window()
     
     yield driver_instance
     
     driver_instance.quit()
+
+# @allure.step('Фикстура: инициализация драйвера браузера')
+# @pytest.fixture
+# def driver():
+#     chrome_options = ChromeOptions()
+#     chrome_options.add_argument("--incognito")
+#     chrome_options.add_argument(f"--window-size=1400,1000")
+#     driver_instance = webdriver.Chrome(options=chrome_options)
+    
+#     yield driver_instance
+    
+#     driver_instance.quit()
 
 @allure.step('Фикстура: инициализация объекта класса SigninPage')
 @pytest.fixture
